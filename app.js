547d501b4e1bc539af2bb269b950bf7c1d87e735
Knave-De-Coeur <alexanderm.1496@gmail.com>
2023-05-01 18:14:15 +0200

    Included express js and some files, started messing with routes.

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
-const http = require('http')
+const express = require('express')
+const path = require('path')
+const app = express()
+const {products, people} = module.require('./data')
+// setup static and middleware
+app.use(express.static('./public'))
 
-
-// behind the scenes emits an event
-const server = http.createServer((req, res) => {
-
-    const url = req.url
-
-    if (url === '/') {
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<h1>Home Page</h1>')
-        res.end()
-    } else if (url === '/about') {
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<h1>About Page</h1>')
-        res.end()
-    } else {
-        res.writeHead(404, {'content-type': 'text/html'})
-        res.write('<h1>Not Found</h1>')
-        res.end()
-    }
+app.get('/', (req, res) => {
 
 })
 
-server.listen(5001, () => {
-    console.log('Server listening on 5001...')
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product;
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productID', (req, res) => {
+
+    const {productID} = req.params;
+
+    const singleProduct = products.find((product) => product.id === Number(productID))
+
+    if (!singleProduct) {
+        return res.status(404).send('Product does not exist')
+    }
+
+    res.json(singleProduct)
+})
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+
+    const {search, limit} = req.query
+    let sortedProducts = [...products];
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+
+    if (sortedProducts.length < 1) {
+        return res.status(200).json()
+    }
+
+    res.status(200).json({success:true, data: sortedProducts})
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send("About Page")
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send("<h1>Resource not found</h1>")
+})
+
+app.listen(5001, () => {
+    console.log("server is running on 5001")
 })
